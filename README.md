5e947916bb69bb90a9d97ca2b1d6d05a69894615
eun0115 <gianpaoloestacio5@gmail.com>
2024-03-31 03:50:21 +0000

    even: Initial skeleton
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android Device tree for realme C25/C25s/Narzo 50A
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_even.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/realme/even/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_even
+PRODUCT_DEVICE := even
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := realme C25/C25s/Narzo 50A
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
