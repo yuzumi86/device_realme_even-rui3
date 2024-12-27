2b99ca4050de1ea5d070278ead7537324d9b4409
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-12-27 22:23:55 +0700

    Aosp

--- aosp_even.mk
+++ aosp_even.mk
@@ -7,15 +7,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 
 # Inherit from device makefile.
 $(call inherit-product, device/realme/even/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_even
+PRODUCT_NAME := aosp_even
 PRODUCT_DEVICE := even
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
